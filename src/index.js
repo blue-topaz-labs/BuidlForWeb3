4924bcce05366c8df9367f8e6b5f05398660214d
Piyush Dhall <contact@piyush.ovh>
2022-11-06 03:24:10 +0530

    updated vercel cors link

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ mongoose.Promise = global.Promise;
 // App Setup
 app.use(
   cors({
-    origin: ["https://buidl-for-web3.vercel.app/", "http://localhost:3000"],
+    origin: ["https://buidl-for-web3.vercel.app", "http://localhost:3000"],
   })
 );
 app.use(morgan("dev"));
